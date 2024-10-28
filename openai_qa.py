02a6775cf07a301b2db4fd710aa50e0d363c31f7
David Shih <david600174@gmail.com>
2024-10-28 15:36:38 +0800

    add process answer(not tested) with openai

--- openai_qa.py
+++ openai_qa.py
@@ -9,6 +9,30 @@ def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
 
+def get_process_answer(description: str, image_path: str): # -> tuple[str, object]:
+    message = f"Provided Image is an UI image. To test this ui, we need to perform the action that {description}. What action shall we do? (output template: {{ \"action\": [click / double-click / long press / scroll], \"position\": [x, y] }})"
+    base64_image = encode_image(image_path)
+    messages = [
+        {"role": "system", "content": "You are an UI analysis assistant."},
+        {
+            "role": "user",
+            "content": [
+                {
+                "type": "text",
+                "text": message,
+                },
+                {
+                "type": "image_url",
+                "image_url": {
+                    "url":  f"data:image/jpeg;base64,{base64_image}"
+                },
+                },
+            ],
+        }
+    ]
+    ans = get_llm_answer(messages)
+    print(ans)
+
 def check_picture_result(description: str, image_path: str):
     message = f"We want to assert the ui provided as image. The image should contain: {description}. Provide us only a similarity_score between 0 to 1. Please only output a floating-point number between 0 and 1 (for example: 0.53)."
     base64_image = encode_image(image_path)
@@ -30,11 +54,14 @@ def check_picture_result(description: str, image_path: str):
             ],
         }
     ]
-    score = 0
-    try:
-        score = float(get_llm_answer(messages))
-    except ValueError:
-        print("Not a float")
+    score = -1
+    err_count = 0
+    while score == -1 and err_count < 5:
+        try:
+            score = float(get_llm_answer(messages))
+        except ValueError:
+            err_count += 1
+            print("llm response not float")
     
     print(score)
     return score
