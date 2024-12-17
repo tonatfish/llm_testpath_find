53f9720986a903f4c43c983a5538434997b2b47f
David Shih <david600174@gmail.com>
2024-12-18 00:36:26 +0800

    add new swipe method (for demo)

--- test/test/test_about/test_about.py
+++ test/test/test_about/test_about.py
@@ -4,6 +4,11 @@ from pathlib import Path
 from PIL import Image
 from appium.webdriver.webdriver import WebDriver
 from appium.webdriver.webelement import WebElement
+from appium.webdriver.common.touch_action import TouchAction
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.common.actions.pointer_input import PointerInput
+from selenium.webdriver.common.actions import interaction
 from helpers import ImageUtils
 from helpers import ShellUtils
 from helpers import DriverUtils
@@ -36,7 +41,7 @@ def test_about(about_driver: WebDriver, tmp_path: Path, language_index: int):
 @pytest.mark.parametrize("language_index", [1, ])
 def test_buttonPolicy(about_driver: WebDriver, tmp_path: Path, language_index: int):
     expected = True
-    about_language_setting(about_driver, language_index)
+    # about_language_setting(about_driver, language_index)
 
     ShellUtils.tap_element(about_driver, elements["buttonPolicy"])
     time.sleep(1)
@@ -45,6 +50,44 @@ def test_buttonPolicy(about_driver: WebDriver, tmp_path: Path, language_index: i
 
     # result = Image.open(str(tmp_path / "buttonPolicy_unscrolled_result.png")).convert('RGB')
     # result.save("samples/about/buttonPolicy_unscrolled_language" + str(language_index) + ".png")
+    
+    about_driver.save_screenshot(
+        str(tmp_path/"buttonPolicy_0.png"))
+    result = Image.open(str(tmp_path / "buttonPolicy_0.png")).convert('RGB')
+    result.save("tmp/buttonPolicy_0.png")
+    action = TouchAction(about_driver)
+    element = elements["textMessage"]
+    swipe_x = element["x"] + element["width"] / 2 + 1
+    swipe_start_y = element["y"] + element["height"] - 1
+    swipe_end_y = element["y"] + 1
+
+    start_time = time.time()
+
+    for i in range(1, 7):
+        # # original swipe with inertia (1.6sec):
+        # DriverUtils.swipe_element_down(about_driver, elements["textMessage"], 200)
+        # # swipe with little inertia (1.1sec):
+        # touch_input = PointerInput(interaction.POINTER_TOUCH, "touch")
+        # actions = ActionChains(about_driver)
+        # actions.w3c_actions = ActionBuilder(about_driver, mouse=touch_input)
+        # actions.w3c_actions.pointer_action.move_to_location(swipe_x, swipe_start_y)
+        # actions.w3c_actions.pointer_action.pointer_down()
+        # actions.w3c_actions = ActionBuilder(about_driver, mouse=touch_input, duration=200)
+        # actions.w3c_actions.pointer_action.move_to_location(swipe_x, swipe_end_y)
+        # actions.w3c_actions.pointer_action.release()
+        # actions.w3c_actions.pointer_action.click_and_hold()
+        # actions.perform()
+        # swipe with no inertia (fully stable) (3 sec):
+        action.press(x=swipe_x, y=swipe_start_y).move_to(x=swipe_x, y=swipe_end_y).release().perform()
+        about_driver.save_screenshot(
+            str(tmp_path/("buttonPolicy_" + str(i) + ".png")))
+        result = Image.open(str(tmp_path / ("buttonPolicy_" + str(i) + ".png"))).convert('RGB')
+        result.save("tmp/buttonPolicy_" + str(i) + ".png")
+        print("--- %s seconds ---" % (time.time() - start_time))
+
+    time.sleep(3)
+
+    assert False
 
     expected = expected and ImageUtils.compare_images_from_file(
         "samples/about/buttonPolicy_unscrolled_language" + str(language_index) + ".png", str(tmp_path / "buttonPolicy_unscrolled_result.png"), 0.998)
